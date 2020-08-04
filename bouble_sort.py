d7f8aa3aa44c2062244167f5eb57d6ca1069e97b
Oleg Tsoy <otsoy@Olegs-MacBook-Air.local>
2020-08-04 14:20:36 -0700

    first

---
+++ bouble_sort.py
@@ -0,0 +1,12 @@
+text = open("/Users/otsoy/Desktop/otsoy/python/numb.txt", "r") #directory of file
+data = list(text.read())
+line = len(data)
+
+print ("unsort", data)
+
+for i in range(line):
+	for j in range(line-1):
+		if data[j] > data[j+1]:
+			data[j], data[j+1] = data[j+1],data[j]
+print("sort", data)
+text.close
